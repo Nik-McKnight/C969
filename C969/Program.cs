11cb99d93abda3c15d3fc028bfa17844e3a31a77
Nik McKnight <nik.mcknight735@gmail.com>
2023-10-02 13:45:14 -0600

    initial commit

---
+++ C969/Program.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace C969
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // Create connection string
+            string server = "localhost";
+            string database = "mydb";
+            string username = "root";
+            string password = "nikk";
+            string constring = "SERVER="+server+";DATABASE="+database+";UID="+username+";PASSWORD="+password+";";
+
+            // Create mysql connection
+            MySqlConnection conn = new MySqlConnection(constring);
+            conn.Open();
+
+            // Query
+            string query = "select * from user";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(0));
+                Console.WriteLine(reader.GetString(1));
+                Console.WriteLine(reader.GetString(2));
+                Console.WriteLine(reader.GetString(3));
+                Console.WriteLine(reader.GetString(4));
+                Console.WriteLine(reader.GetString(5));
+                Console.WriteLine(reader.GetString(6));
+                Console.WriteLine(reader.GetString(7));
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+    }
+}
